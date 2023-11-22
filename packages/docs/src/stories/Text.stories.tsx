6607c67365c298791279cdd1341f312eb105f91d
Nayara Holanda <naayaraholanda@gmail.com>
2023-11-22 17:49:59 -0300

    Add Text component

---
+++ packages/docs/src/stories/Text.stories.tsx
@@ -0,0 +1,20 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Text, TextProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Text',
+  component: Text,
+  args: {
+    children:
+      'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia repellat quod repellendus ipsam? Eius, recusandae sequi officiis, reprehenderit expedita alias animi aperiam nisi in id delectus numquam, impedit autem doloremque?',
+  },
+} as Meta<TextProps>
+
+export const Primary: StoryObj<TextProps> = {}
+
+export const CustomTag: StoryObj<TextProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+}
