6a48a700a2976d64be02cefb6b1384bea06fe5d9
Nayara Piauilino <naayaraholanda@gmail.com>
2024-02-28 15:02:27 -0300

    Add Checkbox component

---
+++ packages/react/src/components/Checkbox/index.tsx
@@ -0,0 +1,20 @@
+import { ComponentProps } from 'react'
+import { CheckboxContainer, CheckboxIndicator } from './styles'
+import { Check } from 'phosphor-react'
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {
+  prefix?: string
+}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator asChild>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  )
+}
+
+Checkbox.displayName = 'Checkbox'
